535ea687d8a5bf9e4fe3a54dc6919bb48b7257e3
DylanDeSchryver <142050322+DylanDeSchryver@users.noreply.github.com>
2024-04-01 22:31:44 -0400

    Update camfeeds.py

--- camfeeds.py
+++ camfeeds.py
@@ -7,73 +7,70 @@ from lineDetect import lineDetection
 
 # Function to update camera feed with overlay
 def update_overlay_feed(top_left_frame):
-    try:
-        url = 'video.mp4'
-        cap = cv2.VideoCapture(url)
 
-        # Function to convert OpenCV image to Tkinter PhotoImage
-        def convert_to_photo_image(frame):
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image = Image.fromarray(image)
-            photo = ImageTk.PhotoImage(image=image)
-            return photo
+    url = 'video.mp4'
+    cap = cv2.VideoCapture(url)
 
-        # Function to update the label within top_left_frame with a new image
-        def update_label():
-            ret, frame = cap.read()
+    # Function to convert OpenCV image to Tkinter PhotoImage
+    def convert_to_photo_image(frame):
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(image)
+        photo = ImageTk.PhotoImage(image=image)
+        return photo
 
-            if ret:
-                frame = cv2.resize(frame,(650,380))
-                photo = convert_to_photo_image(frame)
-                camera_feed_label.configure(image=photo)
-                camera_feed_label.image = photo
-                top_left_frame.after(10, update_label)  # Update every 10 milliseconds
-            else:
-                print("Error reading frame from camera feed")
+    # Function to update the label within top_left_frame with a new image
+    def update_label():
+        ret, frame = cap.read()
 
-        # Create a label within top_left_frame for displaying the camera feed
-        camera_feed_label = tk.Label(top_left_frame)
-        camera_feed_label.pack()
+        if ret:
+            frame = cv2.resize(frame,(650,380))
+            photo = convert_to_photo_image(frame)
+            camera_feed_label.configure(image=photo)
+            camera_feed_label.image = photo
+            top_left_frame.after(10, update_label)  # Update every 10 milliseconds
+        else:
+            print("Error reading frame from camera feed")
 
-        # Start updating the label
-        update_label()
+    # Create a label within top_left_frame for displaying the camera feed
+    camera_feed_label = tk.Label(top_left_frame)
+    camera_feed_label.pack()
 
-    except Exception as e:
-        print(f"Error updating camera feed: {e}")
+    # Start updating the label
+    update_label()
+
+
 
 #Function to display camera feed without overlay
 def update_camera_feed(bottom_left_frame):
-    try:
-        url = 'videoc.mp4'
-        cap = cv2.VideoCapture(url)
 
-        # Function to convert OpenCV image to Tkinter PhotoImage
-        def convert_to_photo_image(frame):
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image = Image.fromarray(image)
-            photo = ImageTk.PhotoImage(image=image)
-            return photo
+    url = 'videoc.mp4'
+    cap = cv2.VideoCapture(url)
 
-        # Function to update the label within top_left_frame with a new image
-        def update_label():
-            ret, frame = cap.read()
+    # Function to convert OpenCV image to Tkinter PhotoImage
+    def convert_to_photo_image(frame):
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(image)
+        photo = ImageTk.PhotoImage(image=image)
+        return photo
 
-            if ret:
-                frame = lineDetection(cap)
-                frame = cv2.resize(frame, (650, 380))
-                photo = convert_to_photo_image(frame)
-                camera_feed_label.configure(image=photo)
-                camera_feed_label.image = photo
-                bottom_left_frame.after(10, update_label)  # Update every 10 milliseconds
-            else:
-                print("Error reading frame from camera feed")
+    # Function to update the label within top_left_frame with a new image
+    def update_label():
+        ret, frame = cap.read()
 
-        # Create a label within top_left_frame for displaying the camera feed
-        camera_feed_label = tk.Label(bottom_left_frame)
-        camera_feed_label.pack()
+        if ret:
+            frame = lineDetection(cap)
+            frame = cv2.resize(frame, (650, 380))
+            photo = convert_to_photo_image(frame)
+            camera_feed_label.configure(image=photo)
+            camera_feed_label.image = photo
+            bottom_left_frame.after(10, update_label)  # Update every 10 milliseconds
+        else:
+            print("Error reading frame from camera feed")
 
-        # Start updating the label
-        update_label()
+    # Create a label within top_left_frame for displaying the camera feed
+    camera_feed_label = tk.Label(bottom_left_frame)
+    camera_feed_label.pack()
 
-    except Exception as e:
-        print(f"Error updating camera feed: {e}")
+    # Start updating the label
+    update_label()
+
